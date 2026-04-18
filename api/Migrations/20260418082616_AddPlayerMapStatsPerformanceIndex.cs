8bd968f0e678a74de44558cd62fee433f9ca243d
sila-skandia <anskia.sailing@gmail.com>
2026-04-18 18:26:38 +1000

    perf(alias-detection): add PlayerMapStats index and parallelize analysis via DI scopes

---
+++ api/Migrations/20260418082616_AddPlayerMapStatsPerformanceIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlayerMapStatsPerformanceIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerMapStats_PlayerName_ServerGuid_Year_Month",
+                table: "PlayerMapStats",
+                columns: new[] { "PlayerName", "ServerGuid", "Year", "Month" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PlayerMapStats_PlayerName_ServerGuid_Year_Month",
+                table: "PlayerMapStats");
+        }
+    }
+}
